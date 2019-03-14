5b72e80e6a30c055295b653988eb97e3c89f8017
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-03-14 14:12:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,48 +1,46 @@
-#require 'rack-flash'
+require 'sinatra/base'
+require 'rack-flash'
 
 class SongsController < ApplicationController
-#  use Rack::Flash
 
-  get '/songs' do
+  enable :sessions
+  use Rack::Flash
+
+  get "/songs" do
     @songs = Song.all
-    erb :'/songs/index'
+    erb :"songs/index"
   end
 
-  get '/songs/new' do
-    erb :'/songs/new'
+  get "/songs/new" do
+    erb :"songs/new"
   end
 
-  get '/songs/:slug' do
+  get "/songs/:slug" do
     @song = Song.find_by_slug(params[:slug])
-
-    erb :'songs/show'
+    erb :"songs/show"
   end
 
-  post '/songs' do
+  post "/songs" do
     @song = Song.create(:name => params["Name"])
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
-    @song.genre_ids = params[:genres]
+    @song.genre_ids = params["genres"]
     @song.save
-
     flash[:message] = "Successfully created song."
-
-    redirect("/songs/#{@song.slug}")
+    redirect to("/songs/#{@song.slug}")
   end
 
-  get '/songs/:slug/edit' do
+  get "/songs/:slug/edit" do
     @song = Song.find_by_slug(params[:slug])
-    erb :'songs/edit'
+    erb :"songs/edit"
   end
 
-  patch '/songs/:slug' do
+  patch "/songs/:slug" do
     @song = Song.find_by_slug(params[:slug])
-    @song.update(params[:song])
-    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    @song.genre_ids = params[:genres]
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params["genres"]
     @song.save
-
     flash[:message] = "Successfully updated song."
-    redirect("/songs/#{@song.slug}")
+    redirect to("/songs/#{@song.slug}")
   end
 
 end
